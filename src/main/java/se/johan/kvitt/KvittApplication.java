c8c0e107fb83b778b58d54894f9576b433a2466b
johanhammerin <johan04hammerin@gmail.com>
2025-10-09 16:09:23 +0200

    initial commit

---
+++ src/main/java/se/johan/kvitt/KvittApplication.java
@@ -0,0 +1,13 @@
+package se.johan.kvitt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvittApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvittApplication.class, args);
+	}
+
+}
