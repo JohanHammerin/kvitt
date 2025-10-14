56251c77cdc413e21c2532e8b34ed40e387a2ea3
johanhammerin <johan04hammerin@gmail.com>
2025-10-14 17:50:39 +0200

    Fler variabler för User
    Skapade totalIncome samt totalExpenses för User

--- src/main/java/se/johan/kvitt/user/model/User.java
+++ src/main/java/se/johan/kvitt/user/model/User.java
@@ -3,10 +3,14 @@ package se.johan.kvitt.user.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.math.BigDecimal;
+
 @Document(collection = "kvittCollection")
 public class User {
     @Id
     private String id;
     private String username;
     private String password;
+    private BigDecimal totalIncome;
+    private BigDecimal totalExpenses;
 }
